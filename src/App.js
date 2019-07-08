7330ab8d816d995cf09b53a3110a14068ae02e05
sumit kumar <sumit1523@gmail.com>
2019-07-09 00:35:11 +0530

    bootstrap Added

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
   const paginate = pageNumber => setCurrentPage(pageNumber);
 
   return (
-    <div className="Container mt-5">
+    <div className="container mt-5">
       <h1 className="text-primary mb-3">My Pagination</h1>
       <Posts posts={currentPosts} loading={loading} />
       <Pagination
